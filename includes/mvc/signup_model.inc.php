c3dae1b329a9dcd554c622f250ac82708fb15400
Jason <devop.jas@gmail.com>
2025-07-12 04:09:19 +0800

    added some php files

---
+++ includes/mvc/signup_model.inc.php
@@ -0,0 +1,34 @@
+<?php 
+
+declare(strict_types=1);
+
+function get_username(object $pdo ,string $username){
+
+    $query = "SELECT username from users WHERE username = :username;";
+    $stmt = $pdo->prepare($query);
+
+    $stmt->bindParam(":username", $username);
+    $stmt->execute();
+    
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return $result;
+}
+
+
+
+function get_email(object $pdo, string $email){
+
+    $query = "SELECT email FROM users WHERE email = :email;"; //query 
+
+    $stmt = $pdo->prepare($query); //prepare
+    $stmt->bindParam(":email",$email);//bind 
+    $stmt->execute(); //execute
+
+    // get the result
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return $result; // return the result from using this function
+
+
+}
---
+++ includes/signup.inc.php
@@ -0,0 +1,61 @@
+<?php 
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+   $username = $_POST["username"];
+   $pwd = $_POST["pwd"];
+   $email = $_POST["email"];
+   
+   try{
+      require_once("dbh.inc.php");
+      require_once("./mvc/signup_model.inc.php");
+      require_once("./mvc/signup_controller.inc.php");
+
+
+      // ERROR HANDLER
+
+      $errors = [];
+      
+
+      if(is_input_empty($username,$pwd,$email)){
+         $errors["empty_input"] = "Fill in all Fields!";
+      }
+
+      if(is_email_invalid($email)){
+          $errors["invalid_email"] = "Invalid email used!";
+      }
+
+      if(is_username_taken($pdo, $username)){
+          $errors["username_taken"] = "username is already taken!";
+      }
+
+
+      if(is_email_registered($pdo,$email)){
+          $errors["email_used"]  = "email is already registerded";
+      }
+ 
+  
+     require_once 'config.inc.php';
+  
+
+      if($errors){
+         $_SESSION["errors_signup"] = $errors;
+      }
+
+   }catch(PDOException $e){
+      die("Query failed: ".$e->getMessage());
+   }
+
+
+   
+
+   // 
+
+   // $query  = "INSERT INTO users (username, email, pwd) VALUES (:username, :email,:pwd);";
+   // $stmt = $pdo->prepare($query);
+
+   //  //hash the password
+
+}else{
+   header("location:../index.php");
+   die();
+}
